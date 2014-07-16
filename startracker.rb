927c73c3d94ab658e62d9101361bc78dd001d08f
Jordan Kueh <jordan@zerocubestudios.com>
2014-07-16 23:36:19 +1000

    Functions! Functions are useful. Also added a .gitignore file, to avoid committing Gemfile.lock.

--- startracker.rb
+++ startracker.rb
@@ -3,24 +3,60 @@ require 'json'
 require 'net/http'
 require 'date'
 
+# Variable Declarations
 consignmentNumber = ARGV[0]
 date = Date::today
-if consignmentNumber == nil
-	puts "Please enter a consignment number."
+ERROR_PREFIX = "[ERROR]"
+ERROR_CONNECTION = "#{ERROR_PREFIX} There was an error connecting to the \
+server. Please check your Internetion connection."
+ERROR_RESPONSE = "#{ERROR_PREFIX} Invalid response from server. Please check \
+that the consignment number is valid."
+ERROR_NOCONSIGNNUM = "#{ERROR_PREFIX} Please enter a valid consignment number."
+
+# Some handy-dandy functions
+def error_noConsignNum
+	puts  ERROR_NOCONSIGNNUM
 	exit 1
 end
-guid = JSON.parse(Net::HTTP.get(
-	"sttrackandtrace.startrack.com.au",
-	"/Consignment/GetConsignmentsBySearchCriteriaShort/#{consignmentNumber}"
-))
+def error_connection
+	puts ERROR_CONNECTION
+	exit 2
+end
+def error_response
+	puts ERROR_RESPONSE
+	exit 3
+end
+
+# Validation
+if consignmentNumber == nil
+	error_noConsignNum
+end
+
+def decode_response(host,uri)
+	begin
+		result = Net::HTTP.get(host,uri)
+	rescue SocketError
+		error_connection
+	end
+	begin
+		result = JSON.parse(result)
+	rescue JSON::ParserError
+		error_response
+	end
+	return result
+end
+
+# Stop yakking and get on with it
 begin
+	guid = decode_response("sttrackandtrace.startrack.com.au",
+		"/Consignment/GetConsignmentsBySearchCriteriaShort/#{consignmentNumber}"
+	)
+	events = decode_response("sttrackandtrace.startrack.com.au",
+		"/Consignment/GetConsignmentEventsByConsignmentGuid/#{guid[0]}"
+	)
 	printf "Events for consignment number #{consignmentNumber} as at "
 	printf "%4i-%02i-%02i",date.year,date.month,date.mday
 	printf ":\n"
-	events = JSON.parse(Net::HTTP.get(
-		"sttrackandtrace.startrack.com.au",
-		"/Consignment/GetConsignmentEventsByConsignmentGuid/#{guid[0]}"
-	))
 	events.each {
 		|event|
 		output = "#{event['EventDate']} @ #{event['Time']}\t#{event['Status']}"
@@ -30,10 +66,8 @@ begin
 			puts "#{output} at #{event['Location']}"
 		end
 	}
-rescue => JSON::ParseError
-	puts "Error while parsing response from the server. Please check that \
-the consignment number is valid."
-	exit 1
 rescue => e
-	puts "#{e.class} #{e.message}"
+	puts "#{ERROR_PREFIX} \
+#{e.class}: #{e.message}"
+	exit 4
 end
